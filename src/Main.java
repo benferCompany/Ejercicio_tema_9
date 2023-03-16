0efe90ab3a2907e01ff5f6635c21e50e9e85dda6
Benjamín Bernabé Gomez <benjidfer@gmail.com>
2023-03-16 11:44:00 -0300

    Ejecicio del tema 9

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Cliente cliente = new Cliente(32,"Benjamin","3624230777",5000);
+        System.out.println("Nombre: "+cliente.nombre+" Edad: "+cliente.edad+" Telefono: "+cliente.telefono+" Credito: "+cliente.credito);
+    }
+
+
+
+}
+
